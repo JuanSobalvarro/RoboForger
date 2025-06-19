968c803faadccb4529c9439cc39a3011543fd620
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-06-19 14:16:04 -0600

    refactor: Figure refactoring

--- RoboForger/drawing/figures/figure.py
+++ RoboForger/drawing/figures/figure.py
@@ -14,44 +14,62 @@ class Figure:
         if not points or len(points) < 2:
             raise ValueError("A figure must have at least two points.")
         self.name = name
-        self.target_count = 0
         self.lifting = lifting
         self.velocity = velocity
         self.float_precision = float_precision
 
         # Round points to the specified float precision
-        self.points: List[Point3D] = []
-        for point in points:
-            rounded_point = tuple(round(coord, float_precision) for coord in point)
-            self.points.append(rounded_point)
-
+        self._points: List[Point3D] = [Figure.round_point(point, self.float_precision) for point in points]
 
         self.__skip_pre_down = False
         self.__skip_end_lifting = False
 
         self.rob_targets = []
-        self.rob_targets_formatted = []
+        self.target_count = 0
 
         # Add lifting points
-        self.add_lifting_points()
+        self._add_lifted_points()
         self.__generate_rob_targets()
+
+    @staticmethod
+    def round_point(point: Point3D, precision: int) -> Point3D:
+        """
+        Rounds the point coordinates to the specified float precision.
+        """
+        if len(point) != 3:
+            raise ValueError("Point must be a tuple of three float values (x, y, z).")
+
+        if not isinstance(precision, int) or precision <= 0:
+            raise ValueError("Precision must be a positive integer.")
+
+        return tuple(round(coord, precision) for coord in point)
+
+    def _add_lifted_points(self):
+        """
+        Adds a lifted point at the start and end of the figure points. Pre-down and end-lifted points
+        """
+        start_lifted_point = (self._points[0][0], self._points[0][1], self._points[0][2] + self.lifting)
+        end_lifted_point = (self._points[-1][0], self._points[-1][1], self._points[-1][2] + self.lifting)
+
+        self._points.insert(0, start_lifted_point)
+        self._points.append(end_lifted_point)
 
     def get_start_and_end_points(self) -> (Point3D, Point3D):
         """
         Returns the first and last points of the figure.
         The first point is the pre-down (lifted) point, and the last point is the end lifted point.
         """
-        if not self.points:
+        if not self._points:
             raise ValueError("The figure has no points.")
-        return self.points[0], self.points[-1]
+        return self._points[0], self._points[-1]
 
     def reverse_points(self):
         """
         Reverses the order of points in the figure.
         This is useful for drawing figures in reverse order.
         """
-        self.points.reverse()
-        self.__generate_rob_targets()
+        self._points.reverse()
+        self.rob_targets.clear()
 
     def set_velocity(self, velocity: int):
         if velocity <= 0:
@@ -85,7 +103,7 @@ class Figure:
         self.__skip_end_lifting = skip
 
     def get_points(self) -> List[Point3D]:
-        return self.points
+        return self._points
 
     @staticmethod
     def offset_coord(origin_target_name: str, origin: Point3D, point: Point3D) -> str:
@@ -103,33 +121,29 @@ class Figure:
         """
         ...
 
-    def get_rob_targets(self) -> List[str]:
-        """
-        Returns the list of all rob targets name in order of use
-        """
-        return self.rob_targets
-
     def clear_rob_targets(self):
         """
         Clears the rob targets list and resets the target count.
         This is useful if you want to regenerate the rob targets.
         """
         self.rob_targets.clear()
-        self.rob_targets_formatted.clear()
         self.target_count = 0
 
-    def get_rob_targets_formatted(self) -> List[str]:
-        if not self.rob_targets_formatted:
-            self.__generate_rob_targets()
-        return self.rob_targets_formatted
+    def get_rob_targets(self) -> List[str]:
+        """
+        Returns the list of all rob targets name in order of use
+        """
+        return self.rob_targets
 
-    def create_robtarget(self, point: Point3D) -> str:
+    def get_rob_targets_formatted(self) -> List[str]:
+        return self.__generate_rob_targets()
+
+    @staticmethod
+    def _create_robtarget_coord(point: Point3D) -> str:
         """
         Creates a rob target string for a given point.
         This is useful for generating individual rob targets without generating the entire list.
         """
-        target_name = self.__generate_target_name(self.target_count)
-        self.target_count += 1
         return f"[[{point[0]},{point[1]},{point[2]}]," \
                f"[4.14816E-8,6.1133E-9,-1,-2.53589E-16],[0,0,-1,0]," \
                f"[9E+9,9E+9,9E+9,9E+9,9E+9,9E+9]]"
@@ -137,44 +151,29 @@ class Figure:
     @staticmethod
     def rob_target_format(target_name: str, point: Point3D) -> str:
         """
-        Formats a rob target string for a given point.
+        Formats a rob target string for a given point and target name.
         This is useful for generating individual rob targets without generating the entire list.
         """
-        return f"CONST robtarget {target_name}:=[[{point[0]},{point[1]},{point[2]}]," \
-               f"[4.14816E-8,6.1133E-9,-1,-2.53589E-16],[0,0,-1,0]," \
-               f"[9E+9,9E+9,9E+9,9E+9,9E+9,9E+9]];\n"
-
-    def add_lifting_points(self):
-        # Pre down point, this point is used as the pre starting point, the robot needs to move here and then down
-        # This is useful for avoiding collisions with the workpiece or other obstacles
-        start_lifted_point = (self.points[0][0], self.points[0][1], self.points[0][2] + self.lifting)
-        self.points.insert(0, start_lifted_point)
-        # Append lift point to the end of the points list
-        self.points.append(
-            (self.points[-1][0], self.points[-1][1], self.points[-1][2] + self.lifting))  # Lift the tool up by 50 units
+        return f"CONST robtarget {target_name}:={Figure._create_robtarget_coord(point)};\n"
 
     def __generate_rob_targets(self):
         """
-        This function generates the rob targets for the figure based on the points provided. It will generate a point lifted,
+        This function generates the rob targets for the figure based on the points the figure has. It will generate a point lifted,
         a point at the start(down), movements of the figure**, then a point lifted at the end.
+
+        ** The generated rob targets are in ORDER ACCORDING TO THE POINTS at the current state of the figure.
         """
         self.rob_targets.clear()
-        self.rob_targets_formatted.clear()
+        rob_targets_formatted: List[str] = []
         self.target_count = 0
 
         for point in self.get_points():
             target_name = self.__generate_target_name(self.target_count)
-            self.rob_targets.append(target_name)
-
-            self.rob_targets_formatted.append(
-                f"CONST robtarget {target_name}:=[[{point[0]},{point[1]},{point[2]}],"
-                f"[4.14816E-8,6.1133E-9,-1,-2.53589E-16],[0,0,-1,0],"
-                f"[9E+9,9E+9,9E+9,9E+9,9E+9,9E+9]];\n"
-            )
             self.target_count += 1
-            # print("Generated robtarget:", target_name, "for point:", point)
 
-        return self.rob_targets_formatted
+            rob_targets_formatted.append(Figure.rob_target_format(target_name, point))
+
+        return rob_targets_formatted
 
     def __generate_target_name(self, count: int) -> str:
         return f"P{self.name}{count}"
